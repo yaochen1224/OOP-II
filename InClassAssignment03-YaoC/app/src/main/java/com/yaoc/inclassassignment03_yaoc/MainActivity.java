2a1918b687b973657454be7738dd9d9aaf795c40
yaochen1224 <yao.chen1224@foxmail.com>
2017-02-09 17:16:58 -0500

    TextView Manipulator

--- InClassAssignment03-YaoC/app/src/main/java/com/yaoc/inclassassignment03_yaoc/MainActivity.java
+++ InClassAssignment03-YaoC/app/src/main/java/com/yaoc/inclassassignment03_yaoc/MainActivity.java
@@ -3,31 +3,41 @@ package com.yaoc.inclassassignment03_yaoc;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.yaoc.inclassassignment03_yaoc.R.id.field;
 
 public class MainActivity extends AppCompatActivity {
 
 
     String text = "";
-    TextView boardTextView;
-    String boardText;
+    EditText boardTextView;
+    TextView fieldTextView;
+    int textSize = 12;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        boardTextView = (TextView) findViewById(R.id.board);
-        boardText = boardTextView.getText().toString();
+        boardTextView = (EditText) findViewById(R.id.board);
+        fieldTextView = (TextView) findViewById(R.id.field);
+        showToast();
     }
 
+    public void showToast() {
+        Toast t = Toast.makeText(this, "This is a Toast Message", Toast.LENGTH_SHORT);
+        t.show();
+    }
 
     public void displayText(String text){
-        TextView displayTextView = (TextView) findViewById(R.id.field);
-        displayTextView.setText(text);
+        fieldTextView.setText(text);
     }
 
     public void copy(View view){
-        text = boardText;
+        text = boardTextView.getText().toString();
         displayText(text);
     }
 
@@ -35,6 +45,37 @@ public class MainActivity extends AppCompatActivity {
         text = " "+text + " "+text;
         displayText(text);
     }
+
+    public void upCase(View view){
+        textSize += 2;
+        fieldTextView.setTextSize(textSize);
+    }
+
+    public void lowerCase(View view){
+        textSize -= 2;
+        fieldTextView.setTextSize(textSize);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("Board Text",boardTextView.getText().toString());
+        outState.putString("Field Text", text);
+        outState.putInt("Field Text Size",textSize);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if(savedInstanceState != null){
+            String str = savedInstanceState.getString("Board Text");
+            text = savedInstanceState.getString("Field Text");
+            textSize = savedInstanceState.getInt("Field Text Size");
+            boardTextView.setText(str);
+            fieldTextView.setText(text);
+            fieldTextView.setTextSize(textSize);
+        }
+    }
+
 
 
 }
