4e13f70e3eba6cc1e86088051f498758a9da319e
yaochen1224 <yao.chen1224@foxmail.com>
2017-02-22 22:05:43 -0500

    new commitment

---
+++ InClassAssignment05-YaoC/app/src/main/java/com/yaoc/inclassassignment05_yaoc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yaoc.inclassassignment05_yaoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
